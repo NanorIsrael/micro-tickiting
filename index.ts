034c0eec3684a15455ddce153724517af030c121
nanorisrael <israel.nanor@compozelabs.com>
2024-07-22 20:40:36 +0000

    Add express server configs

---
+++ index.ts
@@ -0,0 +1,36 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv';
+import path from 'path'
+import morgan from 'morgan';
+import helmet from 'helmet';
+import multer from 'multer';
+
+// // configs
+
+dotenv.config()
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: true, limit: '30mb'}))
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({policy: 'cross-origin'}))
+app.use(morgan('common'))
+app.use(cors())
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')))
+
+// file storage
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, 'public/assets');
+	},
+	filename: function (req, file, cb){
+		cb(null, file.originalname)
+	}
+})
+const upload = multer({ storage })
+
+const PORT = process.env.PORT || 8000
+mongoose.connect((process.env.MONGO_URL) as string).then(() => 
+	app.listen(PORT, () => console.log('Sever running on port ' + PORT))
+).catch((error) => console.log(`${error}`))
