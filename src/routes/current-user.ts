4b2798f3cf15a8f34f2bdf360010e88092dc9527
nanorisrael <israel.nanor@compozelabs.com>
2024-08-27 18:57:10 +0000

    Add signout implementation

--- src/routes/current-user.ts
+++ src/routes/current-user.ts
@@ -1,7 +1,16 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
-router.get("/api/users/currentuser", (req, res) => {
-  res.status(200).send("Hi there");
+router.get("/api/users/current-user", (req, res) => {
+  if (!req.session?.jwt) {
+    return res.status(401).json({ currentUser: null });
+  }
+  try {
+    const user = jwt.verify(req.session?.jwt, process.env.JWT_KEY!);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ currentUser: null });
+  }
 });
 export { router as currentUserRouter };
--- src/routes/signout.ts
+++ src/routes/signout.ts
@@ -1,7 +1,8 @@
 import express from "express";
 
 const router = express.Router();
-router.get("/api/users/signout", (req, res) => {
-  res.status(200).send("Hi there");
+router.post("/api/users/signout", (req, res) => {
+  req.session = null;
+  res.status(200).json({});
 });
 export { router as signoutRouter };
