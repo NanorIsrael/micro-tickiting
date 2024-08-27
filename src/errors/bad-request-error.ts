9a646af0153cc3ed062a603f13d6f1c04293d51d
nanorisrael <israel.nanor@compozelabs.com>
2024-08-27 10:55:14 +0000

    update error handler

---
+++ src/errors/bad-request-error.ts
@@ -0,0 +1,17 @@
+import CustomError from "./custom-error";
+
+class BadRequestError extends CustomError {
+	statusCode: number = 400;
+	constructor(readonly  message: string) {
+		super(message)
+		Object.setPrototypeOf(this, BadRequestError.prototype)
+	}
+
+	serializeError(): { message: string; field?: string | null; }[] {
+		return [{
+			message: this.message,
+			field: null
+		}]
+	}
+}
+export default BadRequestError;
